3a449370ea3df02e013ddcc7da390ef4c25d57ae
filipe augusto <67299107+filipe-augusto@users.noreply.github.com>
2022-08-20 22:12:43 -0300

    Add project files.

---
+++ Default.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="WebForms.Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+            <asp:TextBox ID="idTexto" runat="server"></asp:TextBox>
+            <input id="telefone" name="telfone" type="text"  runat="server"/>
+            <asp:Button ID="btnmostrar" runat="server" OnClick="btnmostrar_Click" Text="Mostrar Mensagem" />
+        </div>
+        <br />
+        <br />
+        <br />
+        <asp:DropDownList ID="ddlContatos" runat="server"></asp:DropDownList>
+        <br />
+        <br />
+        <br />
+        <select id="selectManual">
+            <% foreach (var item in contatos) { %>
+                <option value="<%=item%>"> <%=item%></option>
+                    
+              <% } %>
+
+        </select>
+         <br />
+        <br />
+        <br />
+           <div id="selectManual1" runat="server">
+
+               <asp:GridView ID="GridView1" runat="server">
+               </asp:GridView>
+
+               </div>
+        <asp:GridView ID="GridView2" runat="server">
+        </asp:GridView>
+    </form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace WebForms
+{
+    public partial class Default : System.Web.UI.Page
+    {
+
+        public List<string> contatos = new List<string>();
+        public List<Contato> listaContatos = new List<Contato>();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+            listaContatos.Add(new Contato() { Nome = "Filipe", ID = 1 });
+            listaContatos.Add(new Contato() { Nome = "Cassila", ID = 2 });
+            listaContatos.Add(new Contato() { Nome = "Nice", ID = 3 });
+            listaContatos.Add(new Contato() { Nome = "Isaac", ID = 4 });
+            listaContatos.Add(new Contato() { Nome = "Mathias", ID = 5 });
+
+            GridView2.DataSource = listaContatos;
+            GridView2.DataBind();
+
+            contatos.Add("danilo");
+            contatos.Add("Sheila");
+            contatos.Add("Filipe");
+            contatos.Add("mathias");
+            contatos.Add("billy");
+            contatos.Add("pedro");
+            contatos.Add("augusto");
+            //jeito 1
+            //   ddlContatos.DataSource = contatos;
+            // ddlContatos.DataBind();
+            //jeito 2
+            ddlContatos.DataSource = listaContatos;
+            ddlContatos.DataValueField = "ID";
+            ddlContatos.DataTextField = "Nome";
+            ddlContatos.DataBind();
+
+
+            //jeito 3
+            //selectManual1.InnerHtml += "<select>";
+            //foreach (var item in contatos)
+            //{
+            //    selectManual1.InnerHtml += "<option value='" + item + "' >" + item + "</option>";
+            //}
+            //selectManual1.InnerHtml += "</select>";
+
+            GridView1.DataSource = contatos;
+            GridView1.DataBind();
+        }
+
+        protected void btnmostrar_Click(object sender, EventArgs e)
+        {
+            Response.Write("Olá " + idTexto.Text);
+            Response.Write("Olá" + Request["idTexto"]);
+            //Response.Write("TE")
+
+            Response.Write("<script> alert('" + idTexto.Text + "')</script> ");
+        }
+    }
+
+   public class Contato
+    {
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
